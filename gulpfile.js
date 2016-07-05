899791c655efedd408bbd13d48d966f5d77ec6b2
Rodrigo Souza <ro_web1@hotmail.com>
2016-07-05 17:24:51 -0300

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,37 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function () {
-  return gulp.src('dist/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+// gulp.task('sass', function () {
+//   return gulp.src('dist/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('css'));
+// });
+//
+// gulp.task('watch', function () {
+//   gulp.watch('dist/**/*.scss', ['sass']);
+// });
+
+//-------------------------------------------
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("dist/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task('watch', function () {
-  gulp.watch('dist/**/*.scss', ['sass']);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("dist/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve']);
