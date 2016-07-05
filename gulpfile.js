6e0be39112ec3df49359915100674487a72d468c
Rodrigo Souza <ro_web1@hotmail.com>
2016-07-05 16:03:10 -0300

    Now is Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,5 @@
 var gulp = require('gulp');
-var stylus = require('gulp-stylus');
 
-gulp.task('default', ['one']);
-
-// Get one .styl file and render
-gulp.task('one', function () {
-  return gulp.src('assets/LojaSite.styl')
-    .pipe(stylus())
-    .pipe(gulp.dest('css'));
+gulp.task('default', function(){
+  //laksdhkajsdghas
 });
