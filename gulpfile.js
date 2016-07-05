c0fa863c2df404eb3427cc30ca4098e13c305eea
Rodrigo Souza <ro_web1@hotmail.com>
2016-07-05 17:31:29 -0300

    Prepare for Jeet

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,6 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
-// gulp.task('sass', function () {
-//   return gulp.src('dist/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('css'));
-// });
-//
-// gulp.task('watch', function () {
-//   gulp.watch('dist/**/*.scss', ['sass']);
-// });
-
-//-------------------------------------------
-
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -28,6 +16,15 @@ gulp.task('serve', ['sass'], function() {
 });
 
 // Compile sass into CSS & auto-inject into browsers
+// gulp.task('sass', function() {
+//     return gulp.src("dist/*.scss")
+//         .pipe(sass({
+//           use:jeet()
+//         }))
+//         .pipe(gulp.dest("css"))
+//         .pipe(browserSync.stream());
+// });
+
 gulp.task('sass', function() {
     return gulp.src("dist/*.scss")
         .pipe(sass())
