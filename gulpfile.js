c8641672dae0022553775c4085c9c0009e7acafd
Rodrigo Souza <ro_web1@hotmail.com>
2016-07-06 11:15:15 -0300

    gulpfile improvements and more jeet

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,33 @@
 var gulp = require('gulp'),
   //sass = require('gulp-sass');
   stylus = require('gulp-stylus'),
+  prefixer = require('gulp-autoprefixer'),
+  rupture = require('rupture'),
+  jeet = require('jeet'),
   browserSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['stylus'], function() {
 
     browserSync.init({
-        server: "./"
+      // Serve files from the app directory with directory listing
+      server: {
+          baseDir: "./",
+          index: 'rodrigo1.html'
+      }
+
     });
 
     gulp.watch("dist/*.styl", ['stylus']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-// Compile sass into CSS & auto-inject into browsers
-// gulp.task('sass', function() {
-//     return gulp.src("dist/*.scss")
-//         .pipe(sass({
-//           use:jeet()
-//         }))
-//         .pipe(gulp.dest("css"))
-//         .pipe(browserSync.stream());
-// });
-
 gulp.task('stylus', function() {
     return gulp.src("dist/*.styl")
-        .pipe(stylus())
+        .pipe(stylus({
+          use:[jeet()],
+          compress: false
+        }))
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
